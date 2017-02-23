07440bda0a5809e5d212ca5f7db964d846b17efd
Gon Diaz Cornejo <gondiazc@gmail.com>
2017-02-23 13:55:29 +0000

    Fixed spelling errors on contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,9 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      name = param[:contact][:name]
-      email = param[:contact][:email]
-      body = param[:contact][:comments]
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent."
       redirect_to new_contact_path
