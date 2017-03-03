315de21511a36eb98823a8400de063dfcdabce1d
Gon Diaz Cornejo <gondiazc@gmail.com>
2017-03-03 16:04:54 +0000

    Added js for sending card fields to Stripe

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,24 @@
+/* global $, Stripe */
+// Document ready.
+$(document).on('turbolinks:load', function() {
+  var theForm = $('#pro_form');
+  var signupBtn = $('#form-signup-btn');
+  // Set Stripe public key.
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // When user clicks form submit btn, prevent default submission behavior.
+  signupBtn.click(function(event){
+    event.preventDefault();
+    // Collect the credit card fields.
+    var ccNum = $('#card_number').val(),
+      cvcNum = $('#card_code').val(),
+      expMonth = $('#card_month').val(),
+      expYear = $('#card_year').val();
+    // Send the card info to Stripe.
+    Stripe.createToken({
+      number: ccNum,
+      cvc: cvcNum,
+      exp_month: expMonth,
+      exp_year: expYear
+    }, stripeResponseHandler);
+  });
+});
